cff69e8571fcd51dbedc3f17fd707d839fbe95b7
lnusfaumer <lnusfaumer@gmail.com>
2020-09-16 14:21:45 +1000

    creacion commit inicial

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
-const express = require ('express');
-const path = require('path');
-const app = express();
+const express = require ('express')
+const app = express()
+const path = require('path')
+
 
 app.get('/', function (req, res) {
-    let file = path.resolve('vistas/index.html');
-    res.sendFile(file);
-});
+    let file = path.resolve('vistas/index.html')
+    res.sendFile(file)
+})
 
 app.get('*', function(req, res) {
 	if (req.url.endsWith('css')) {
 	    let file = path.resolve('public' + req.url);
-	    return res.sendFile(file);
-	};
+	    return res.sendFile(file)
+	}
+
 	let image = ['jpg', 'jpeg', 'gif', 'png', 'svg'];
 	let ext = req.url.split('.')[1];
 	if (image.includes(ext)) {
 	    let file = path.resolve('public' + req.url);
 	    return res.sendFile(file);
-	};
-});
+	}
+
+})
 app.listen(3000);
